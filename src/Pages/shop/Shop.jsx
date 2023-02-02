c83852bbf7ad33734a3a78310b5c38fae68c48bd
Joeybervin <61996287+Joeybervin@users.noreply.github.com>
2023-02-02 17:56:34 +0100

    Création de la data
    Mise en place de la "base de données" en dure

---
+++ src/Pages/shop/Shop.jsx
@@ -0,0 +1,7 @@
+export default function Shop() {
+    return (
+        <div >
+            <h1>Shop page</h1>
+        </div>
+    );
+}
---
+++ src/assets/index.js
@@ -0,0 +1,38 @@
+/* bagmunchkingrey */
+import bagmunchkingrey1 from './products/bags/bagmunchkingrey/bagmunchkingrey1.webp';
+import bagmunchkingrey2 from './products/bags/bagmunchkingrey/bagmunchkingrey2.webp';
+import bagmunchkingrey3 from './products/bags/bagmunchkingrey/bagmunchkingrey3.webp';
+import bagmunchkingrey4 from './products/bags/bagmunchkingrey/bagmunchkingrey4.webp';
+import bagmunchkingrey5 from './products/bags/bagmunchkingrey/bagmunchkingrey5.webp';
+import bagmunchkingrey6 from './products/bags/bagmunchkingrey/bagmunchkingrey6.webp';
+
+/* baggoldeneyesred1 */
+import baggoldeneyesred1 from './products/bags/baggoldeneyesred/baggoldeneyesred1.webp';
+import baggoldeneyesred2 from './products/bags/baggoldeneyesred/baggoldeneyesred2.webp';
+import baggoldeneyesred3 from './products/bags/baggoldeneyesred/baggoldeneyesred3.webp';
+import baggoldeneyesred4 from './products/bags/baggoldeneyesred/baggoldeneyesred4.webp';
+
+/* backpackexplorerlight */
+import backpackexplorerlight1 from './products/bags/backpackexplorerlight/backpackexplorerlight1.webp';
+import backpackexplorerlight2 from './products/bags/backpackexplorerlight/backpackexplorerlight2.webp';
+import backpackexplorerlight3 from './products/bags/backpackexplorerlight/backpackexplorerlight3.webp';
+import backpackexplorerlight4 from './products/bags/backpackexplorerlight/backpackexplorerlight4.webp';
+import backpackexplorerlight5 from './products/bags/backpackexplorerlight/backpackexplorerlight5.webp';
+import backpackexplorerlight6 from './products/bags/backpackexplorerlight/backpackexplorerlight6.webp';
+
+/* phoneiphonepurple */
+import phoneiphonepurple1 from './products/bags/phoneiphonepurple/phoneiphonepurple1.webp';
+import phoneiphonepurple2 from './products/bags/phoneiphonepurple/phoneiphonepurple2.webp';
+import phoneiphonepurple3 from './products/bags/phoneiphonepurple/phoneiphonepurple3.webp';
+
+
+
+const bagMunchkinGrey = {bagmunchkingrey1, bagmunchkingrey2, bagmunchkingrey3, bagmunchkingrey4, bagmunchkingrey5, bagmunchkingrey6};
+const bagGoldenEyesRed = {baggoldeneyesred1, baggoldeneyesred2, baggoldeneyesred3, baggoldeneyesred4};
+const backpackExplorerLight = { backpackexplorerlight1, backpackexplorerlight2,backpackexplorerlight3, backpackexplorerlight4, backpackexplorerlight5, backpackexplorerlight6};
+const phoneIphonePurple = {phoneiphonepurple1, phoneiphonepurple2, phoneiphonepurple3};
+/* EXPORT ALL */
+
+
+export default {bagMunchkinGrey, bagGoldenEyesRed, backpackExplorerLight, phoneIphonePurple};
+
