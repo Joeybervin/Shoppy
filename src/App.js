9711753de842e747bb37b23dee7bbe27c5f56d32
Joeybervin <61996287+Joeybervin@users.noreply.github.com>
2023-02-02 14:48:02 +0100

    Création de toutes les pages
    Initialisation des dossier des pages qui composerons le projet, les components se feront au fur et à mesure que le projet avance.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import './App.css';
-import {Routes, Route} from "react-router-dom";
 
 /* Pages */
-import Home from "./Pages/Home/Home"
-import About from "./Pages/About/About"
+import About from "./Pages/about/About";
+import Home from "./Pages/home/Home";
+import Shop from "./Pages/shop/Shop";
+import Product from "./Pages/shop/product/Product";
+import Cart from "./Pages/cart/Cart";
+import Contact from "./Pages/contact/Contact";
 
 /* Component */
 import NavBar from './Components/NavBar';
@@ -14,9 +18,20 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
+        {/* Home */}
         <Route path="/" element={< Home />}/>
+        {/* About */}
         <Route path="/about" element={< About />} />
-        <Route path="*" element={< Home />} />
+        {/* Contact */}
+        <Route path="/contact" element={< Contact />} />
+        {/* Shop A REVOIR */}
+        <Route path="/shopping" element={< Shop />}>
+          <Route path="/shoping/product/:category/:type/:refProduct" element={< Product />}/>
+        </Route>
+        {/* Cart */}
+        <Route path="/cart" element={< Cart />} />
+
+        <Route path="*" element={< Home />}/>
       </Routes>
     </div>
   );
